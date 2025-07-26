36a23f6de1930f38f1145e7b9e849a376da320e6
Mehraveh-Nejabati42 <mehraveh.nejabati42@gmail.com>
2025-07-26 10:42:49 +0330

    i dont know how to end it

--- A and B.py
+++ A and B.py
@@ -1,41 +1,32 @@
 import random
 sum1=0
 sum2=0
-i1=1
-i=1
-dice2=0
 while sum1<100 or sum2<100:
-    player1=input("player A press space to roll the dice:")
-    if player1==" ":
-        dice1=random.randint(1,6)
-    print("dice=",dice1)
-    for i1 in range(1,101):
-        print(i1,"   " ,end=" ")
-        if i1%10==0:
+    pa=str(input("press space to roll the dice player A:")) #player A's turn
+    dice_a=random.randint(1,6)
+    print("dice=",dice_a)
+    sum1+=dice_a
+    if pa==" ":
+        for i in range(1,101):
+            if sum1==i:
+                print("A",end="")
+            if sum2==i:
+                print("B",end="")
+            print(i,end="    ")
+            if i%10==0:
                 print()
-        if sum1+dice1>100:
-            continue
-        elif i1==dice1:
-            print("A", end=" ")
-            sum1+=dice1
-        elif i==dice2:
-            print("B", end=" ")
-            sum2+=dice2
-            
-    player2=input("player B press space to roll the dice:")
-    if player2==" ":
-        dice2=random.randint(1,6)
-    print("dice=",dice2)
-    for i in range(1,101):
-        print(i,"   " ,end=" ")
-        if i%10==0:
+
+    pb=str(input("press space to roll the dice player B:"))  #player B's turn
+    dice_b=random.randint(1,6)
+    print("dice=",dice_b)
+    sum2+=dice_b
+    if pb==" ":
+        for j in range(1,101):
+            if sum2==j:
+                print("B",end="")
+            if sum1==j:
+                print("A",end="")
+            print(j,end="    ")
+            if j%10==0:
                 print()
-        if sum2+dice2>100:
-            print("its a bigger number")
-            continue
-        elif i==dice2:
-            print("B", end=" ")
-            sum2+=dice2
-        elif i1==dice1:
-            print("A", end=" ")
-            sum1+=dice1
+        
